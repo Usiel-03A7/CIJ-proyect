613cf9c76a86fb7b49b4c384b03504603f3500ff
Usiel-03A7 <shebastyan@gmail.com>
2023-03-07 21:55:45 -0600

    making carrousel with hooks useState useEffect

--- src/components/carrousel/carrousel.jsx
+++ src/components/carrousel/carrousel.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './carrousel.css';
 
 export default function Carrousel() {
+  const [img, setImg] = useState(1);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (img > 6) {
+        setImg(1);
+      } else {
+        setImg(img + 1);
+      }
+    }, 5000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [img]);
   return (
     <div className="containerCarrousel">
       <figure className="deslizar">
-        <p>Anterior</p>
-        <img src="/imgCarrousel/img1.jpeg" alt="" />
-        <p>Siguiente</p>
+        <img src={`/imgCarrousel/img${img}.jpeg`} alt="" />
       </figure>
     </div>
   );
