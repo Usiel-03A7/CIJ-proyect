16742f7fa1ac54b35fe1fef8c6dc9c662147198c
Noel-S <noel.silva.v@gmail.com>
2023-03-09 19:35:31 -0600

    Adding useMemo hook to prevent object passed in Provider changes in every re render

--- src/components/wrapper/index.jsx
+++ src/components/wrapper/index.jsx
@@ -1,15 +1,18 @@
-import React, {useState} from "react"
-import NavColorContext from "../../contexts/nav_color"
-import Nav from "../navbar/Nav";
-import Carrousel from "../carrousel/carrousel";
-function Wrapper(){
-  const  [navColor, setNavColor] = useState('trasnparent')
-  return(
-    <NavColorContext.Provider value={{navColor,setNavColor}}>
-      <Nav />
-      <Carrousel setNavColor={setNavColor}/>
-    </NavColorContext.Provider>
+import React, { useMemo, useState } from 'react';
+import NavColorContext from '../../contexts/nav_color';
+import Nav from '../navbar/Nav';
+import Carrousel from '../carrousel/carrousel';
 
-  )
+function Wrapper() {
+  const [navColor, setNavColor] = useState('trasnparent');
+  const navColorMemo = useMemo(() => ({ navColor, setNavColor }), [navColor, setNavColor]);
+
+  return (
+    <NavColorContext.Provider value={navColorMemo}>
+      <Nav />
+      <Carrousel setNavColor={setNavColor} />
+    </NavColorContext.Provider>
+  );
 }
+
 export default Wrapper;
