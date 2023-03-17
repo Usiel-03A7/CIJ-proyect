ae5f13a2dc4edd6c37fea2985ff90c65278fdf34
Usiel-03A7 <shebastyan@gmail.com>
2023-03-17 17:08:02 -0600

    make a navegationbar with the arrowdown or dropdown

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -8,15 +8,20 @@ function Nav() {
       {({ navColor }) => (
         <nav className={['navbar', navColor].join(' ')}>
           <img src="./logo.jpeg" alt="" />
-          <ul>
+          <a href="#menu" className='nav-target'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+            </svg>
+          </a>
+          <ul className='nav_links' id='menu'>
             <li>
-              <a href="#home">Home</a>
+              <a className='nav_link' href="#home">Anuncios</a>
             </li>
             <li>
-              <a href="#about">About</a>
+              <a className='nav_link' href="#about">Conocenos</a>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <a className='nav_link' href="#contact">Contactanos</a>
             </li>
           </ul>
         </nav>
