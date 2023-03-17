e0d943700f748e6709fea13f46899a67773957fc
Usiel-03A7 <shebastyan@gmail.com>
2023-03-17 16:59:42 -0600

    generate article and information about cij

--- src/components/article/index.jsx
+++ src/components/article/index.jsx
@@ -3,16 +3,17 @@ import './article.css';
 
 export default function Article() {
   return (
-    <article>
-      <div className="articleHead">
-        <h2>Brindamos platicas y orientaciones con el objetivo de ser mejores personas</h2>
+    <article >
+      <div className="articleHead" >
+        <h2>CIJ Cuenta con una red integrada por más de 8 mil voluntarios que anualmente atienden a poco más de 5 millones de personas a nivel nacional.</h2>
       </div>
+      <div id='about'></div>
       <div className="articleBottom">
         <img src="" alt="" />
-        <div className="articleBottomContent">
-          <p>Titulo</p>
-          {/* eslint-disable-next-line max-len */}
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam, porro delectus sint adipisci architecto quia, a necessitatibus vel tempore in, nemo perferendis culpa. Reiciendis, voluptatum autem. Soluta eaque quo beatae?</p>
+        <div className="articleBottomContent" >
+          <p >¿Quienes somos?</p>
+          
+          <p>Centros de Integración Juvenil, A.C. (CIJ) es una asociación civil no lucrativa incorporada al Sector Salud fundada en 1969, con 53 años de experiencia en la atención integral del consumo de drogas, está conformada por una red de 120 unidades operativas en todo el país.</p>
         </div>
         <img src="" alt="" />
       </div>
