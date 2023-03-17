c8ad487b81a68273e7839d7324caf00b06e6b31a
Usiel-03A7 <shebastyan@gmail.com>
2023-03-17 17:08:36 -0600

    add the tag for use all components

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Wrapper from './components/wrapper';
 import Article from './components/article';
+import Contactanos from './components/contactanos';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Wrapper />
     <Article />
+    <Contactanos />
   </React.StrictMode>,
 );
