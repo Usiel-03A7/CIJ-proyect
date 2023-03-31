f3a051c45e67dbb91398fc75fd01b74a20e84d11
Usiel-03A7 <shebastyan@gmail.com>
2023-03-31 07:14:06 -0600

    start to main and add the tags of anothers compoments

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Wrapper from './components/wrapper';
 import Article from './components/article';
+import Avisos from './components/avisos';
+import Footer from './components/footer';
 import Contactanos from './components/contactanos';
 import './index.css';
 
@@ -9,6 +11,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Wrapper />
     <Article />
+    <Avisos />
     <Contactanos />
+    <Footer />
   </React.StrictMode>,
 );
