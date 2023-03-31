0a825e7f7f6e6fb5e200894d781694281e59eb0e
Usiel-03A7 <shebastyan@gmail.com>
2023-03-31 07:12:34 -0600

    start navbar and responcibe menu

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -3,19 +3,27 @@ import NavColorContext from '../../contexts/nav_color';
 import './Nav.css';
 
 function Nav() {
+  
+  const openMenu = () => {
+    location.href='#menu'
+  }
+  
   return (
     <NavColorContext.Consumer>
       {({ navColor }) => (
         <nav className={['navbar', navColor].join(' ')}>
           <img src="./logo.jpeg" alt="" />
-          <a href="#menu" className='nav-target'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
+          <button className='nav-target' onClick={openMenu}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
             </svg>
-          </a>
+          </button>
           <ul className='nav_links' id='menu'>
             <li>
-              <a className='nav_link' href="#home">Anuncios</a>
+              <a className='nav_link' href="#carrousel">Inicio</a>
+            </li>
+            <li>
+              <a className='nav_link' href="#anuncios">Anuncios</a>
             </li>
             <li>
               <a className='nav_link' href="#about">Conocenos</a>
