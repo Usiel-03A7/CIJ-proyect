f6a5183b5b65ed2435c98a367d33e8ca997489a9
Noel-S <noel.silva.v@gmail.com>
2023-06-12 12:05:34 -0600

    Render Article items from firebase in home

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,16 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
 import Wrapper from '../wrapper';
 import Article from '../article';
 import Avisos from '../avisos';
 import Footer from '../footer';
 import Contactanos from '../contactanos';
-export default function Home (){
+
+import app from '../../data/firebase';
+
+export default function Home() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    const db = getFirestore(app);
+    const getArticles = async () => {
+      const snapshot = await getDocs(collection(db, 'articles'));
+      const data = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setArticles(data);
+    };
+    getArticles();
+  }, []);
   return (
-  <>
-   <Wrapper />``
-    <Article />
-    <Avisos />
-    <Contactanos />
-    <Footer />
-  </>
-  )
+    <>
+      <Wrapper />
+      {articles.map((article) => (
+        <Article
+          key={article.id}
+          title={article.title}
+          subtitle={article.subtitle}
+          text={article.text}
+        />
+      ))}
+      <Avisos />
+      <Contactanos />
+      <Footer />
+    </>
+  );
 }
