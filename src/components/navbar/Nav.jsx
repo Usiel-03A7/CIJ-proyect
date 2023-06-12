d5d00bbd53d0314f37f105341d57dd7904cbd3a0
Noel-S <noel.silva.v@gmail.com>
2023-06-12 12:50:21 -0600

    Replacing class by clasName in components

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -3,33 +3,32 @@ import NavColorContext from '../../contexts/nav_color';
 import './Nav.css';
 
 function Nav() {
-  
   const openMenu = () => {
-    location.href='#menu'
-  }
-  
+    location.href = '#menu';
+  };
+
   return (
     <NavColorContext.Consumer>
       {({ navColor }) => (
         <nav className={['navbar', navColor].join(' ')}>
           <img src="./logo.jpeg" alt="" />
-          <button className='nav-target' onClick={openMenu}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+          <button className="nav-target" onClick={openMenu} type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
             </svg>
           </button>
-          <ul className='nav_links' id='menu'>
+          <ul className="nav_links" id="menu">
             <li>
-              <a className='nav_link' href="#carrousel">Inicio</a>
+              <a className="nav_link" href="#carrousel">Inicio</a>
             </li>
             <li>
-              <a className='nav_link' href="#anuncios">Anuncios</a>
+              <a className="nav_link" href="#anuncios">Anuncios</a>
             </li>
             <li>
-              <a className='nav_link' href="#about">Conocenos</a>
+              <a className="nav_link" href="#about">Conocenos</a>
             </li>
             <li>
-              <a className='nav_link' href="#contact">Contactanos</a>
+              <a className="nav_link" href="#contact">Contactanos</a>
             </li>
           </ul>
         </nav>
