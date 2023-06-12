9ad2e747c95bdd0a2553594109f033aca6929cb9
Noel-S <noel.silva.v@gmail.com>
2023-06-12 12:51:16 -0600

    Replacing keywords svg

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,20 +1,20 @@
-import './footer.css'
+import React from 'react';
+import './footer.css';
 
 export default function Footer() {
   return (
-    <div className='container-fluter'>
-
+    <div className="container-fluter">
       <div>
-        <h2 class="txt-white">Directorio</h2>
+        <h2 className="txt-white">Directorio</h2>
       </div>
 
-      <div className='footer-info'> <h3 class="polololo">Dra. Carmen Fernández Cáceres Directora General de CIJContacto: cfernandez@cij.gob.mx </h3>
-        <h3 class="polololo">Dr. Ángel Prado GarcíaDirector de Operación y Patronatosaprado@cij.gob.mx</h3>
+      <div className="footer-info">
+        <h3 className="polololo">Dra. Carmen Fernández Cáceres Directora General de CIJContacto: cfernandez@cij.gob.mx </h3>
+        <h3 className="polololo">Dr. Ángel Prado GarcíaDirector de Operación y Patronatosaprado@cij.gob.mx</h3>
 
-        <h3 class="polololo">Mtro. Bruno Diaz NegreteDirectivo Normativodireccion.normativa@cij.gob.mx</h3>
-        <h3 class="polololo">Lic. Iván Rubén Rétiz Márquez
-          Director Administrativoivan.retiz@cij.gob.mx</h3></div>
+        <h3 className="polololo">Mtro. Bruno Diaz NegreteDirectivo Normativodireccion.normativa@cij.gob.mx</h3>
+        <h3 className="polololo">Lic. Iván Rubén Rétiz Márquez Director Administrativoivan.retiz@cij.gob.mx</h3>
+      </div>
     </div>
-  )
-
+  );
 }
