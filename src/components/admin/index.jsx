6de97aa9181c079c57fb72e4f5ba9393ba6e0f1f
Usiel-03A7 <shebastyan@gmail.com>
2023-08-10 14:00:39 -0600

    making the more articles

--- src/components/admin/index.jsx
+++ src/components/admin/index.jsx
@@ -23,39 +23,7 @@ export default function Admin() {
  // Es un hook de la libreria React router que permite navegar en las rutas del proyecto
   const navigate = useNavigate();
 
-  // se ejecuta al detactar un cambio en las dependencias del componenete
-  // las dependencias puede ser variables y el array de dependencias es el segundo parametro del useEffect
-  // el primer parametro es una funcion Callback que se ejecutara cuando estas dependencias cambie 
-  // si el array se deja vacio significa que el callback se ejecutara solo una vez al renderizar el componente 
 
-  // useEffect(() => {
-
-
-
-
-
-    
-  //   if (!auth.currentUser) {
-  //     // verifica si el usuario inicio sesion 
-  //     // si no hay un usuario existente se regresa a raiz (home)
-  //     navigate('/', {
-  //       replace: true
-  //     })
-  //     return
-  //   }
-  //   // Cuando un usuario acceda correctamente, podrás obtener información acerca de él en el observador.
-  //   // y escucha cambios en la sesion en caso de que se cierre o inicie session
-  //   onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       const uid = user.uid;
-
-  //     } else {
-  //       navigate('/', {
-  //         replace: true
-  //       })
-  //     }
-  //   });
-  // }, [])cles
   function handdleLogOut (){
     auth.signOut()
   }
